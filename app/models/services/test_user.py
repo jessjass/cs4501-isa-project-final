dcf31bd2e7b2fc8cfa3ea5e617cff66a3e3c3a56
John Mourad <john.m.mourad@gmail.com>
2017-02-28 19:32:04 -0500

    Add Event test case

--- app/models/services/test_user.py
+++ app/models/services/test_user.py
@@ -1,27 +1,35 @@
 from django.test import TestCase, Client, RequestFactory
 from django.core.urlresolvers import reverse
 from services.models import Experience, User, Event
+from django.test import Client
+import json
 
 # class testCaseOne(TestCase):
 #     def test_book(self):
 #         self.assertEqual(1,2)
 
-# class getUserDetailsTestCase(TestCase):
-#     def setUp(self):
-#         self.factory = RequestFactory()
-#         self.user = User.objects.create_user(
-#             firstName='bob', lastName='builder', username = 'bobbuilds', password='cool_stuff') 
+class getUserDetailsTestCase(TestCase):
+    def setUp(self):
+        #self.factory = RequestFactory()
+        #self.c = Client()
+        self.user = User.objects.create(
+            firstName='bob', lastName='builder', username = 'bobbuilds', password='cool_stuff') 
 
-#     def success_response(self):
-#         # Create an instance of a GET request.
-#         request = self.factory.get('/v1/user/1/')
+    def testsuccess(self):
+        # Create an instance of a GET request.
 
-#         # Recall that middleware are not supported. You can simulate a
-#         # logged-in user by setting request.user manually.
-#         request.user = self.user
+        # request = self.factory.get('/v1/user/1/')
 
-#         self.assertEqual(request.user.firstName, self.user.firstName)
+        response = self.client.get(reverse('getById', kwargs={'user_id':1}))
 
-#     #tearDown method is called after each test
-#     def tearDown(self):
-#         pass #nothing to tear down
+        # Recall that middleware are not supported. You can simulate a
+        # logged-in user by setting request.user manually.
+
+        # request.user = self.user
+        dc = json.loads(response.content.decode("utf-8"))[0]['fields']
+        print (dc['firstName'])
+        self.assertEquals(response.status_code, 200)
+
+    #tearDown method is called after each test
+    def tearDown(self):
+        pass #nothing to tear down
