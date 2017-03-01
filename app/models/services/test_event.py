b706e21be894ba791591b51b3b946dc5550d3f61
JeffCui55 <jeffcui55@gmail.com>
2017-02-28 19:45:04 -0500

    test object checks

--- app/models/services/test_event.py
+++ app/models/services/test_event.py
@@ -6,16 +6,16 @@ import json
 
 class TestEvent(TestCase):
 	def setUp(self):
-		Event.objects.create(
+		self.testEvent = Event.objects.create(
 			title="Eat tacos", description="Now", price=10.00, datetime="2017-03-01T12:00:00Z")
 		pass
 
 	def test_all_events(self):
-		response = self.client.get(reverse('event'))
+		response = self.client.get('/api/v1/event/')
 		response = json.loads(response.content.decode('utf-8'))
 
 		print(str(response[0]['fields']['title']))
-		self.assertEqual(response[0]['fields']['title'], 'Eat tacs')
+		self.assertEqual(response[0]['fields']['title'], self.testEvent.title)
 		# self.assertTrue(response.context, '400')
 
 	def tearDown(self):
