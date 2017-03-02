b90707f9a015523a3949efee63c64220135c63c4
Jessica Jassal <jj5dp@virginia.edu>
2017-03-01 21:57:53 -0500

    Tests other than the remove and forms.Form for users are done

--- app/models/services/test_user.py
+++ app/models/services/test_user.py
@@ -2,33 +2,106 @@ from django.test import TestCase, Client, RequestFactory
 from django.core.urlresolvers import reverse
 from services.models import Experience, User, Event
 from django.test import Client
+from services.forms import UserFormUpdateExperience, UserFormUpdateEvent, UserFormUpdateFriend
 import json
 
 # class testCaseOne(TestCase):
 #     def test_book(self):
 #         self.assertEqual(1,2)
 
-class getUserDetailsTestCase(TestCase):
+class TestUser(TestCase):
+
     def setUp(self):
-        #self.factory = RequestFactory()
-        #self.c = Client()
-        self.user = User.objects.create(
-            firstName='bob', lastName='builder', username = 'bobbuilds', password='cool_stuff') 
+        self.user1 = User.objects.create(
+            firstName='bob', lastName='builder', username = 'bobbuilds', password='cool_stuff')
+        self.user2 = User.objects.create(
+            firstName='bill', lastName='billy', username = 'bbills', password='bumblebee')
+        self.testExperience1 = Experience.objects.create(
+            title="Exp 1", description="Not fun", totalPrice=19.99)
+        self.testEvent1 = Event.objects.create(
+            title="Go to the mall", description="Walk around and get food", price=10.00, datetime="2017-03-01T12:00:00Z")
 
-    def testsuccess(self):
-        # Create an instance of a GET request.
+    def testGetUserAll(self):
+        resp = self.client.get('/api/v1/user/').json()
+        
+        self.assertEqual(resp[0]['fields']['username'], self.user1.username)
+        self.assertEqual(resp[1]['fields']['username'], self.user2.username)
 
-        # request = self.factory.get('/v1/user/1/')
+    def testGetUserById(self):
+        resp = self.client.get('/api/v1/user/{0}/'.format(self.user1.pk)).json()
 
-        response = self.client.get(reverse('getById', kwargs={'user_id':1}))
+        self.assertEqual(resp[0]['fields']['username'], self.user1.username)
 
-        # Recall that middleware are not supported. You can simulate a
-        # logged-in user by setting request.user manually.
+    def testPostUserById(self):
+        form = {
+            'firstName':'jessi',
+            'lastName': 'jassal',
+            'username': 'jessij',
+            'password': 'jj'
+        }
+        resp = self.client.post('/api/v1/user/{0}/'.format(self.user1.pk), form).json()
 
-        # request.user = self.user
-        dc = json.loads(response.content.decode("utf-8"))[0]['fields']
-        # print (dc['firstName'])
-        self.assertEquals(response.status_code, 200)
+        self.assertEqual(resp['result'], '200')
+        self.assertEqual(resp['user'][0]['fields']['username'], form['username'])
+
+    def testAddExpUserById(self):
+        # form_data = {'exp_id': '0', 'remove': 'FALSE'}
+        # form = UserFormUpdateExperience(data=form_data)
+        # resp = self.client.post('/api/v1/user/experience/{0}/'.format(self.user1.pk), form).json()
+        
+        # self.assertEqual(resp['result'], '200')
+        pass
+
+    def testAddExpUserByIdRemove(self):
+        # form_data = {'exp_id': '0', 'remove': 'TRUE'}
+        # form = UserFormUpdateExperience(data=form_data)
+        # resp = self.client.post('/api/v1/user/experience/{0}/'.format(self.user1.pk), form).json()
+        
+        # self.assertEqual(resp['result'], '200')
+        pass
+
+    def testAddEventUserById(self):
+        # form_data = {'event_id': '0', 'remove': 'FALSE'}
+        # form = UserFormUpdateEvent(data=form_data)
+        # resp = self.client.post('/api/v1/user/event/{0}/'.format(self.user1.pk), form).json()
+        
+        # self.assertEqual(resp['result'], '200')
+        pass    
+
+    def testAddEventUserByIdRemove(self):
+        # form_data = {'event_id': '0', 'remove': 'TRUE'}
+        # form = UserFormUpdateEvent(data=form_data)
+        # resp = self.client.post('/api/v1/user/event/{0}/'.format(self.user1.pk), form).json()
+        
+        # self.assertEqual(resp['result'], '200')
+        pass   
+
+    def testAddFriendUserById(self):
+        # form_data = {'friend_id': '0', 'remove': 'FALSE'}
+        # form = UserFormUpdateFriend(data=form_data)
+        # resp = self.client.post('/api/v1/friend/event/{0}/'.format(self.user1.pk), form).json()
+        
+        # self.assertEqual(resp['result'], '200')
+        pass
+
+    def testAddFriendUserByIdRemove(self):
+        # form_data = {'friend_id': '0', 'remove': 'TRUE'}
+        # form = UserFormUpdateFriend(data=form_data)
+        # resp = self.client.post('/api/v1/friend/event/{0}/'.format(self.user1.pk), form).json()
+        
+        # self.assertEqual(resp['result'], '200')
+        pass    
+
+    def testRemoveUser(self):
+        form = {
+            'user_id':self.user1.pk
+        }
+
+        resp = self.client.post('/api/v1/user/remove/', form).json()
+        resp2 = self.client.post('/api/v1/user/remove/', form).json()
+        
+        self.assertEqual(resp['result'], '200')
+        # self.assertEqual(resp2['result'], '404')
 
     #tearDown method is called after each test
     def tearDown(self):
