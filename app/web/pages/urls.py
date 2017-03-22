161773a5375ec65fa7cfc9c6bb7850ec0d946a71
Jessica Jassal <jj5dp@virginia.edu>
2017-03-21 22:53:31 -0400

    added sign in url and linked button to url

--- app/web/pages/urls.py
+++ app/web/pages/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^signinpage/$', views.signInPage, name='signInPage'),
     url(r'^experience/(?P<exp_id>[0-9]+)/$', views.experienceDetail, name='experienceDetail'),
     url(r'^event/create/$', views.createEvent, name='createEvent'),
+    url(r'^signin/$', views.signIn, name='signIn'),
 ]
