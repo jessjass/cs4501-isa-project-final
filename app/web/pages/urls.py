231a39db2d7f9b322a495350fb654905b0a520c9
johnmourad <john.m.mourad@gmail.com>
2017-04-08 11:35:42 -0400

    Add search results page

--- app/web/pages/urls.py
+++ app/web/pages/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='home'),
-    url(r'^signin/$', views.signIn, name='signIn'),
     url(r'^experience/(?P<exp_id>[0-9]+)/$', views.experienceDetail, name='experienceDetail'),
     url(r'^event/create/$', views.createEvent, name='createEvent'),
     url(r'^signin/$', views.signIn, name='signIn'),
     url(r'^signup/$', views.signUp, name='signUp'),
     url(r'^signout/$', views.signOut, name='signOut'),
     url(r'^user/dashboard/$', views.userDashboard, name='userDashboard'),
+    url(r'^event/search/$', views.searchEvents, name='searchEvents'),
 ]
