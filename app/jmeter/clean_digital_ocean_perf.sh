e8e16dfb2457ab0bfc9a71646ae043d676915a96
johnmourad <john.m.mourad@gmail.com>
2017-05-03 17:03:11 -0400

    Add Digital Ocean perf tests

---
+++ app/jmeter/clean_digital_ocean_perf.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rm digital-ocean-perf-results.log
+rm -rf digital-ocean-perf-results/
---
+++ app/jmeter/run_digital_ocean_perf.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+jmeter -n -t digital-ocean-perf-test.jmx -l digital-ocean-perf-results.log -e -o digital-ocean-perf-results
