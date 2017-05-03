9011bffd02fb990e974874bdb8979df83ea2955f
johnmourad <john.m.mourad@gmail.com>
2017-05-03 17:06:56 -0400

    Add web2 to before_script and travis

--- before_script.sh
+++ before_script.sh
@@ -10,7 +10,7 @@ docker exec -it mysql-cmdline bash -c "mysql -u root -p'\$3cureUS' -h db -e 'CRE
 docker exec -it mysql-cmdline bash -c "mysql -u root -p'\$3cureUS' -h db -e 'CREATE USER \"www\"@\"%\" IDENTIFIED BY \"\$3cureUS\";' "
 docker exec -it mysql-cmdline bash -c "mysql -u root -p'\$3cureUS' -h db -e 'GRANT ALL ON cs4501.* TO \"www\"@\"%\";' "
 docker exec -it mysql-cmdline bash -c "mysql -u root -p'\$3cureUS' -h db -e 'GRANT ALL ON test_cs4501.* TO \"www\"@\"%\";' "
-docker-compose up -d models exp web kafka es batch lb selenium-chrome
+docker-compose up -d models exp web web2 kafka es batch lb selenium-chrome
 sleep 10
 docker-compose up jmeter
 docker-compose up selenium
